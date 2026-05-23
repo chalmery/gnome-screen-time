6b3572086d00a2540cd5a25fcf082985e6baca75
chalmery <chaochaoycc@gmail.com>
2026-05-23 11:06:46 +0800

    feat: init GSettings and translations in extension entry

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import GLib from 'gi://GLib';
 import { PanelIndicator } from './panelIndicator.js';
 import { PopupWidget } from './popupWidget.js';
 import { UsageTracker } from './usageTracker.js';
@@ -6,11 +7,19 @@ import { UsageStore } from './usageStore.js';
 
 export default class ScreenTimeExtension extends Extension {
     enable() {
-        this._store = new UsageStore();
+        this._settings = this.getSettings();
+
+        let lang = this._settings.get_string('language');
+        if (lang !== 'default') {
+            GLib.setenv('LANGUAGE', lang, true);
+            GLib.setenv('LC_MESSAGES', lang, true);
+        }
+
+        this._store = new UsageStore(this._settings);
         this._indicator = new PanelIndicator();
         this._indicator.addToPanel(this.uuid);
         this._popup = new PopupWidget(this._indicator.menu, this._store);
-        this._tracker = new UsageTracker(this._store);
+        this._tracker = new UsageTracker(this._store, this._settings);
     }
 
     disable() {
@@ -22,5 +31,6 @@ export default class ScreenTimeExtension extends Extension {
         this._indicator = null;
         this._store?.destroy();
         this._store = null;
+        this._settings = null;
     }
 }

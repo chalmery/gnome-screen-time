7789ce53383d7b2e3b703a239cd7ce00de6c802f
chalmery <chaochaoycc@gmail.com>
2026-05-23 11:31:00 +0800

    fix: correct ExtensionPreferences import path for GNOME Shell 50 prefs

--- prefs.js
+++ prefs.js
@@ -1,7 +1,7 @@
 import Gio from 'gi://Gio';
 import Gtk from 'gi://Gtk';
 import Adw from 'gi://Adw';
-import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 export default class ScreenTimePreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
